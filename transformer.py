76305b6d8d36090e2071b7d1cf50b920f0502115
Víctor Santoja Callejo <victorjose.santoja@euskaltel.com>
2024-03-26 10:23:39 +0100

    Create tab for each status and task

--- transformer.py
+++ transformer.py
@@ -4,19 +4,32 @@ from drawio.page import Page
 
 from xml.etree.ElementTree import tostring
 import yaml
+import json
+import os
+
 
 if __name__ == '__main__':
 
     with open('bot.yaml', 'r') as file:
         prime_service = yaml.safe_load(file)
+    with open('bot.json', 'w') as json_file:
+        json.dump(prime_service, json_file)
+    
+    flow_json = json.load(open('bot.json'))
 
-        name_flow = (prime_service[Constants.INBOUND_SHORT_MESSAGE][Constants.NAME])
-        states = (prime_service[Constants.INBOUND_SHORT_MESSAGE][Constants.STATES])
-        tasks = (prime_service[Constants.INBOUND_SHORT_MESSAGE][Constants.TASKS])
+    name_flow = (flow_json[Constants.INBOUND_SHORT_MESSAGE][Constants.NAME])
+    states = (flow_json[Constants.INBOUND_SHORT_MESSAGE][Constants.STATES])
+    tasks = (flow_json[Constants.INBOUND_SHORT_MESSAGE][Constants.TASKS])
 
-        document = Document(name_flow);
-        page1 = Page(states[0][Constants.STATE][Constants.NAME], states[0][Constants.STATE][Constants.REFID])
-        document.add_page(page1)
+    document = Document(name_flow);
+    for state in states:
+        page = Page("State--" + state[Constants.STATE][Constants.NAME], state[Constants.STATE][Constants.REFID])
+        document.add_page(page)
 
-        with open(name_flow + '.drawio', 'w') as f:
-            f.write(tostring(document.to_xml()).decode())
+    for task in tasks:
+        page = Page("Task--" + task[Constants.TASK][Constants.NAME], task[Constants.TASK][Constants.REFID])
+        document.add_page(page)
+
+    with open(name_flow + '.drawio', 'w') as f:
+        f.write(tostring(document.to_xml()).decode())
+        os.remove('bot.json')
