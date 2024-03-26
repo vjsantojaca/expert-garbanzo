fd7bca249ba9e98182aedea0cf0e9a72538f1306
Víctor Santoja Callejo <victorjose.santoja@euskaltel.com>
2024-03-26 12:18:07 +0100

    Refactor line file

--- drawio/line.py
+++ drawio/line.py
@@ -10,15 +10,15 @@ class LineStrokeStyle(enum.Enum):
     DOTTED = 'dotted'
 
 class Line(Element):
-    def __init__(self, points: list[Union[tuple[int, int], Element, tuple[Element, int, int]]],
+    def __init__(self, start: Element, end: Element,
                     stroke_thickness: int = 1, stroke_color: str = "#000000",
                     stroke_style: LineStrokeStyle = LineStrokeStyle.SOLID,
                     rounded: bool = False, curved: bool = False,
                     content: str = "",
                     end_arrow: str = "classic"):
         super().__init__()
-        assert len(points) >= 2
-        self.points: points
+        self.start = start
+        self.end = end
         self.stroke_thickness = stroke_thickness
         self.stroke_color = stroke_color
         self.stroke_style: LineStrokeStyle = stroke_style
@@ -28,8 +28,6 @@ class Line(Element):
         self.end_arrow = end_arrow
 
     def to_xml(self):
-        # TODO deal with None id
-        # TODO parent id
         cell_style = {
             'html': 1,
             "strokeWidth": self.stroke_thickness,
@@ -51,19 +49,9 @@ class Line(Element):
             "value": self.content,
             "edge": "1",
             "parent": "1",
+            "source": self.start.id,
+            "target": self.end.id,
         }
-        source_point = self.points[0]
-        target_point = self.points[len(self.points) - 1]
-        if isinstance(source_point, Element) or len(source_point) == 3:
-            element = source_point if isinstance(source_point, Element) else source_point[0]
-            cell_attrib['source'] = element.id
-            if not isinstance(source_point, Element):
-                cell_style.update(exitX=str(source_point[1]), exitY=str(source_point[2]), exitDx="0", exitDy="0")
-        if isinstance(target_point, Element) or len(target_point) == 3:
-            element = target_point if isinstance(target_point, Element) else target_point[0]
-            cell_attrib['target'] = element.id
-            if not isinstance(target_point, Element):
-                cell_style.update(entryX=str(target_point[1]), entryY=str(target_point[2]), entryDx="0", entryDy="0")
 
         cell_attrib['style'] = ";".join([f"{key}={val}" for (key, val) in cell_style.items()])
 
@@ -77,29 +65,5 @@ class Line(Element):
         }
 
         geometry = SubElement(cell, "mxGeometry", attrib=geometry_attrib)
-
-        if not isinstance(source_point, Element) and len(source_point) == 2:
-            SubElement(geometry, "mxPoint", attrib={
-                "x": str(source_point[0]),
-                "y": str(source_point[1]),
-                "as": "sourcePoint",
-            })
-
-        if not isinstance(target_point, Element) and len(target_point) == 2:
-            SubElement(geometry, "mxPoint", attrib={
-                "x": str(target_point[0]),
-                "y": str(target_point[1]),
-                "as": "targetPoint",
-            })
-
-        if len(self.points) > 2:
-            array = SubElement(geometry, "Array", attrib={
-            "as": "points",
-            })
-            for p in self.points[1:-1]:
-                SubElement(array, "mxPoint", attrib={
-                    "x": str(p[0]),
-                    "y": str(p[1]),
-                })
 
         return cell
