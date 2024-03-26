14f8358057df3bec368c05d43b0dbef3eceddc7d
Víctor Santoja Callejo <victorjose.santoja@euskaltel.com>
2024-03-26 12:18:29 +0100

    Add populate_page_from_actions function

--- transformer.py
+++ transformer.py
@@ -1,4 +1,7 @@
+from drawio.line import Line
+from drawio.rectangle import Rectangle
 from utils.constants import Constants
+from drawio.diamond import Diamond
 from drawio.document import Document
 from drawio.page import Page
 
@@ -7,9 +10,29 @@ import yaml
 import json
 import os
 
+def populate_page_from_actions(actions, page):
+    previous_element = None
+    y_position = 0
+    for action in actions:
+        for key, value in action.items():
+            if key == Constants.DECISION_TASK:
+                element = Diamond(x=0, y=y_position, width=40, height=40, content=value[Constants.NAME])
+            elif key == Constants.TRANSFERTOACD_TASK or key == Constants.CALL_TASK:
+                element = Rectangle(x=0, y=y_position, width=40, height=40, content=value[Constants.NAME], rounded=True)
+            else :
+                element = Rectangle(x=0, y=y_position, width=40, height=40, content=value[Constants.NAME])
+
+            if previous_element is not None:
+                line = Line(previous_element, element)
+                page.add_content(line)
+
+            page.add_content(element)
+
+            previous_element = element
+            y_position += 50
+
 
 if __name__ == '__main__':
-
     with open('bot.yaml', 'r') as file:
         prime_service = yaml.safe_load(file)
     with open('bot.json', 'w') as json_file:
@@ -24,10 +47,12 @@ if __name__ == '__main__':
     document = Document(name_flow);
     for state in states:
         page = Page("State--" + state[Constants.STATE][Constants.NAME], state[Constants.STATE][Constants.REFID])
+        populate_page_from_actions(state[Constants.STATE][Constants.ACTIONS], page)
         document.add_page(page)
 
     for task in tasks:
         page = Page("Task--" + task[Constants.TASK][Constants.NAME], task[Constants.TASK][Constants.REFID])
+        populate_page_from_actions(task[Constants.TASK][Constants.ACTIONS], page)
         document.add_page(page)
 
     with open(name_flow + '.drawio', 'w') as f:
